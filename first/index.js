6c791d08994aa408712b9546b5c3d740be85f9c4
qiqi0303 <sdh220123@sdh.hs.kr>
2024-04-08 14:50:15 +0900

    !

--- first/index.js
+++ first/index.js
@@ -141,8 +141,31 @@ function rectangularColision( { rectangle1, rectangle2} ) {
         rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
         rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
         rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
-    )
+    );
 }
+
+let timer = 10;
+
+function decreaseTimer() {
+    if (timer > 0) {
+        setTimeout(decreaseTimer, 1000);
+        timer--;
+        document.querySelector("#timer"),innerHTML = timer;
+    }
+    if(timer === 0)
+    {
+        document.querySelector("#displayText").style.display = "flex";
+        if(player.health === enemy.health){
+            document.querySelector("#displayText").innerHTML = "무승부";
+        } else if(player.health > enemy.health){
+            document.querySelector("#displayText").innerHTML = "1p승리";
+        } else if(player.health < enemy.health){
+            document.querySelector("#displayText").innerHTML = "2p승리";
+        }
+    }
+}
+
+decreaseTimer();
 
 function animate() {
     window.requestAnimationFrame(animate);
